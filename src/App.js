2898066570891399a5079a0b2514192cdf74fb19
Dariusz Zajac <darekdmc@gmail.com>
2020-05-18 15:03:17 +0200

    Czysty szkielet aplikacji - reszta już przez approvale wzajemne

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>Hello world!</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
