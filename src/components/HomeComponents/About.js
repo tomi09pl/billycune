d97d4de2333871fe94eef720164474064155b927
Leszek <lzbikowski23@gmail.com>
2020-05-30 11:21:11 +0200

    about up2

--- src/components/HomeComponents/About.js
+++ src/components/HomeComponents/About.js
@@ -14,16 +14,23 @@ export default class About extends React.Component {
                         <img src={Image}/>
                     </div>
                     <div className="right">
-                        <NavLink exact to="/portfolio">PORTFOLIO</NavLink>
-                        <Link
-                            activeClass="active"
-                            to="home-contact-container"
-                            spy={true}
-                            smooth={true}
-                            offset={-70}
-                            duration= {900}
-                            className="home-nav-list-link"
-                        >ORDER ART</Link>
+                        <div className="text">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus ad alias animi assumenda at blanditiis, consectetur cum cumque delectus doloribus dolorum eius expedita illo incidunt ipsam ipsum mollitia nemo nostrum odio omnis placeat praesentium quasi qui quidem quis quisquam quos ratione recusandae similique suscipit vel veritatis vitae voluptates. Accusamus adipisci assumenda dignissimos earum eligendi excepturi fuga ipsa maiores quaerat ut.</p>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt ducimus eos esse iusto, neque quisquam vitae. Aliquid cum doloribus exercitationem facilis maiores obcaecati, ut voluptates!</p>
+                        </div>
+
+                        <div className="buttons">
+                            <NavLink exact to="/portfolio">PORTFOLIO</NavLink>
+                            <Link
+                                activeClass="active"
+                                to="home-contact-container"
+                                spy={true}
+                                smooth={true}
+                                offset={-70}
+                                duration= {900}
+                                className="home-nav-list-link"
+                            >ORDER ART</Link>
+                        </div>
                     </div>
                 </div>
             </div>
