e16d5eeaa05208e9b0de57c6161876aef77ddb7f
Leszek <lzbikowski23@gmail.com>
2020-05-30 11:11:32 +0200

    about up1

--- src/components/HomeComponents/About.js
+++ src/components/HomeComponents/About.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import {Link} from "react-scroll";
 import {NavLink} from "react-router-dom";
+import Image from "../../assets/KNUCKLE_web.jpg";
+
 
 export default class About extends React.Component {
     render() {
         return(
-            <div className="home-about-us-container" id="about-us">About art & me
-                <NavLink exact to="/portfolio">PORTFOLIO</NavLink>
-                <Link
-                    activeClass="active"
-                    to="home-contact-container"
-                    spy={true}
-                    smooth={true}
-                    offset={-70}
-                    duration= {900}
-                    className="home-nav-list-link"
-                >ORDER ART</Link>
+            <div className="home-about-us-container" id="about-us">
+                <h1>HOW TO ORDER ART</h1>
+                <div className="main">
+                    <div className="left">
+                        <img src={Image}/>
+                    </div>
+                    <div className="right">
+                        <NavLink exact to="/portfolio">PORTFOLIO</NavLink>
+                        <Link
+                            activeClass="active"
+                            to="home-contact-container"
+                            spy={true}
+                            smooth={true}
+                            offset={-70}
+                            duration= {900}
+                            className="home-nav-list-link"
+                        >ORDER ART</Link>
+                    </div>
+                </div>
             </div>
         )
     }
