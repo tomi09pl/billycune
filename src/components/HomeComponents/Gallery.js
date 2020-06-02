fd2c93e32d4b78dfd303a8960251df4e4e4722d9
tomi09pl <tomi09pl@gmail.com>
2020-06-02 12:01:40 +0100

    Home Gallery poczatek

--- src/components/HomeComponents/Gallery.js
+++ src/components/HomeComponents/Gallery.js
@@ -1,9 +1,89 @@
 import React from 'react';
 
+import img1 from '../../assets/gallery/picture1_vertical.jpg'
+import img2 from '../../assets/gallery/picture2_horizontal.jpg'
+
 export default class Gallery extends React.Component {
     render() {
-        return(
-            <div className="home-gallery-container" id="gallery">Gallery Showcase from Portfolio</div>
+        return (
+            <div className="home-gallery-container" id="gallery">
+                <div className="firstRow galleryRow">
+                    <div className="responsive">
+                        <div className="gallery">
+                            <a href={img1}>
+                                <img src={img1} alt="engine1" width="600" height="400"/>
+                            </a>
+                            <div className="desc">Add a description of the image here</div>
+                        </div>
+                    </div>
+
+                    <div className="responsive">
+                        <div className="gallery">
+                            <a href={img2}>
+                                <img src={img2} alt="engine1" width="600" height="400"/>
+                            </a>
+                            <div className="desc">Add a description of the image here</div>
+                        </div>
+                    </div>
+
+                    <div className="responsive">
+                        <div className="gallery">
+                            <a href={img1}>
+                                <img src={img1} alt="engine1" width="600" height="400"/>
+                            </a>
+                            <div className="desc">Add a description of the image here</div>
+                        </div>
+                    </div>
+
+                    <div className="responsive">
+                        <div className="gallery">
+                            <a href={img2}>
+                                <img src={img2} alt="engine1" width="600" height="400"/>
+                            </a>
+                            <div className="desc">Add a description of the image here</div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="secondRow galleryRow">
+                    <div className="responsive">
+                        <div className="gallery">
+                            <a href={img1}>
+                                <img src={img1} alt="engine1" width="600" height="400"/>
+                            </a>
+                            <div className="desc">Add a description of the image here</div>
+                        </div>
+                    </div>
+
+                    <div className="responsive">
+                        <div className="gallery">
+                            <a href={img2}>
+                                <img src={img2} alt="engine1" width="600" height="400"/>
+                            </a>
+                            <div className="desc">Add a description of the image here</div>
+                        </div>
+                    </div>
+
+                    <div className="responsive">
+                        <div className="gallery">
+                            <a href={img1}>
+                                <img src={img1} alt="engine1" width="600" height="400"/>
+                            </a>
+                            <div className="desc">Add a description of the image here</div>
+                        </div>
+                    </div>
+
+                    <div className="responsive">
+                        <div className="gallery">
+                            <a href={img2}>
+                                <img src={img2} alt="engine1" width="600" height="400"/>
+                            </a>
+                            <div className="desc">Add a description of the image here</div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         )
     }
 }
